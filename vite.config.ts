fa6d0a16602111f376a55f6b16f65ff61d23da88
nirajgirixd <nirajgiri9860@gmail.com>
2026-01-02 08:37:15 +0545

    configure app base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/swift-words/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
